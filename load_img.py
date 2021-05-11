b7afcc4ee33ad55a18cd881ddb7ae35909b42d54
Damiano Malafronte <damiano.malafronte@iit.it>
2021-05-11 11:25:59 +0200

    debug changes

--- load_img.py
+++ load_img.py
@@ -1,11 +1,17 @@
 
 import cv2 as cv
 import numpy as np
+import torch
 
 a = cv.imread("/home/IIT.LOCAL/dmalafronte/sample.jpg")
 
 if a is None:
     a = np.zeros((500,500))
 
-print("Test-VSCode-2")
-print(a.shape)
+print("Test Print")
+print("Image shape: ", a.shape)
+
+tensor = torch.from_numpy(a)
+tensor2 = torch.matmul(tensor, tensor)
+
+print(tensor2.size())
